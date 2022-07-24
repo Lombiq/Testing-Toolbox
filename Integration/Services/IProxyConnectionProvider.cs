d3bd1856c6ff391ff50cfcb26831d2b373e4925d
József Horváth <jozsef.horvath@lombiq.com>
2022-07-24 22:33:17 +0200

    Adding reverse proxy service.

---
+++ Integration/Services/IProxyConnectionProvider.cs
@@ -0,0 +1,19 @@
+using System.Net.Http;
+
+namespace Lombiq.Tests.Integration.Services;
+
+/// <summary>
+/// Represents an object that can resolve the required values for the <see cref="TestReverseProxy"/> instance.
+/// </summary>
+public interface IProxyConnectionProvider
+{
+    /// <summary>
+    /// Gets the url prefix for where to forward the request to.
+    /// </summary>
+    string BaseAddress { get; }
+
+    /// <summary>
+    /// Creates the HTTP client used to forward the request to.
+    /// </summary>
+    HttpClient CreateClient();
+}
