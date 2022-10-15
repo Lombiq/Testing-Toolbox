a292d1e83f18f63fbb28d6747a69cec227b41339
Dávid El-Saig <david.el-saig@lombiq.com>
2022-10-15 15:57:37 +0200

    Add an AutoMocker controller.

---
+++ Integration/Controllers/AutoMockerController.cs
@@ -0,0 +1,24 @@
+﻿using Lombiq.Tests.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Moq.AutoMock;
+
+namespace Lombiq.Tests.Integration.Controllers;
+
+public class AutoMockerController : Controller
+{
+    public string Environment { get; set; } = Environments.Development;
+    public AutoMockerController(AutoMocker mocker)
+    {
+        mocker.Use(this);
+
+        var hostEnvironmentMock = new Mock<IHostEnvironment>();
+        hostEnvironmentMock
+            .SetupGet(hostEnvironment => hostEnvironment.EnvironmentName)
+            .Returns(() => Environment);
+        mocker.Use(hostEnvironmentMock.Object);
+
+        ControllerContext = MockHelper.CreateMockControllerContextWithUser(mocker);
+    }
+}
