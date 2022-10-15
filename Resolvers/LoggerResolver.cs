2de5712840e55ef2c27de11341bcc664595d6e31
Dávid El-Saig <david.el-saig@lombiq.com>
2022-10-15 15:58:07 +0200

    Add generic type resolution to AutoMocker.

---
+++ Resolvers/LoggerResolver.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq.AutoMock;
+using System;
+
+namespace Lombiq.Tests.Resolvers;
+
+public class LoggerResolver : GenericTypeResolver
+{
+    protected override Type ExpectedType { get; } = typeof(ILogger<>);
+
+    protected override object ResolveValue(AutoMocker mocker, Type serviceType, Type genericType)
+    {
+        var factory = mocker.Get<LoggerFactory>();
+        if (factory == null)
+        {
+            mocker.MockLogging();
+            factory = mocker.Get<LoggerFactory>() ?? throw new InvalidOperationException("Couldn't mock logging!");
+        }
+
+        return factory.CreateLogger(genericType.Name);
+    }
+}
